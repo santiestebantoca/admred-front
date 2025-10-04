adea41c1be93d16ff9f62da0d886882563522310
Jorge Lino Santiesteban Toca <jorge.santiesteban@etecsa.cu>
2025-10-03 23:56:09 -0500

    feat: item link logic without scope restriction

--- src/views/item/ItemRelatedItems.vue
+++ src/views/item/ItemRelatedItems.vue
@@ -8,29 +8,19 @@ const items = computed(() => {
   const authUser = useAuthStore().authUser
   const items = []
   if (data.value.padre) {
-    const enabled =
-      authUser.AR ||
-      data.value.padre.origen_id === authUser.area ||
-      data.value.padre.destino_id === authUser.area
     items.push({
       relation: '[P]',
       name: data.value.padre.codigo,
-      enabled,
       state: data.value.padre.estado,
-      to: enabled ? { query: { item: data.value.padre.id } } : ''
+      to: { query: { item: data.value.padre.id } }
     })
   }
   data.value.hijos.forEach(hijo => {
-    const enabled =
-      authUser.AR ||
-      authUser.area === hijo.origen_id ||
-      authUser.area === hijo.destino_id
     items.push({
       relation: '[H]',
       name: hijo.codigo,
-      enabled,
       state: hijo.estado,
-      to: enabled ? { query: { item: hijo.id } } : ''
+      to: { query: { item: hijo.id } }
     })
   })
   return items
@@ -43,9 +33,7 @@ const items = computed(() => {
       <tr v-for="item in items" :key="item.name">
         <td v-text="item.relation" class="text-dark" />
         <td>
-          <router-link :to="item.to" class="text-decoration-none" :class="{ disabled: !item.enabled }">
-            {{ item.name }}
-          </router-link>
+          <router-link :to="item.to" class="text-decoration-none" v-text="item.name" />
         </td>
         <td v-text="item.state" class="text-dark" />
       </tr>
