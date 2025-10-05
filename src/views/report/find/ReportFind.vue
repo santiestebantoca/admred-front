f9f09c836030bdae514d5b2b8c7c0a19c2567cef
Jorge Lino Santiesteban Toca <jorge.santiesteban@etecsa.cu>
2025-10-05 12:19:57 -0500

    fix: update search functionality and improve user feedback in ReportFind.vue

--- src/views/report/find/ReportFind.vue
+++ src/views/report/find/ReportFind.vue
@@ -6,7 +6,7 @@ const title = inject('page:title')
 const report = useReportStore()
 const codigo = ref(null)
 const data = computed(() => report.buscar.data)
-const disabled = computed(() => !(codigo.value || '').match(/^s-\d\d-\d\d\d\d$/i))
+const disabled = computed(() => !codigo.value)
 watch(codigo, () => report.buscar.reset())
 const submit = () => report.buscar.get(codigo.value)
 report.buscar.reset()
@@ -16,14 +16,14 @@ title.value = 'Buscar código'
 <template>
   <div class="pt-3">
     <p>
-      Buscar solicitud por su código.
+      Buscar solicitud por su código (las 10 primeras coincidencias).
     </p>
     <div class="my-3 py-3 border-top border-bottom">
       <form @submit.prevent="submit">
         <div class="d-flex flex-wrap gap-3">
           <div style="width: 160px">
             <!-- <label class="form-label">No. solicitud</label> -->
-            <input v-model="codigo" type="text" class="form-control" placeholder="Código completo" />
+            <input v-model="codigo" type="text" class="form-control" placeholder="Código" />
           </div>
           <div class="hstack align-items-end">
             <bs-btn @click="submit" :disabled="disabled" color="primary" label="Buscar" />
@@ -32,8 +32,10 @@ title.value = 'Buscar código'
       </form>
     </div>
     <div v-if="data">
-      <div v-if="data.id">
-        <bs-btn color="primary" flat :to="{ query: { item: data.id } }" v-text="data.codigo" />
+      <div v-if="data.length" class="hstack gap-3 flex-wrap">
+        <template v-for="d in data">
+          <bs-btn color="primary" flat :to="{ query: { item: d.id } }" v-text="d.codigo" />
+        </template>
       </div>
       <div v-else>
         Código <span class="fw-bold" v-text="codigo" /> no encontrado.
